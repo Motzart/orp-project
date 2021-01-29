8cd800a01c6e2d9ada0b0dba6ec5440dafaf7881
Yurii Oleshko <yurii.oleshko@gmail.com>
2021-01-29 17:20:49 +0200

    fixed markup

--- src/screen/ReforeData.js
+++ src/screen/ReforeData.js
@@ -5,12 +5,21 @@ const ReforeData = () => {
     return (
         <Container>
             <Form.Group as={Row} controlId="formPlaintextPassword">
-                <Form.Label column sm="4">Scope of Project (SURFACE AREA )</Form.Label>
-                <Form.Label column sm="4">100 sq</Form.Label>
+                <Form.Label column sm="4">Surface Area, sq.km</Form.Label>
+                <Form.Label column sm="4">100</Form.Label>
             </Form.Group>
             <Form.Group as={Row} controlId="formPlaintextPassword">
                 <Form.Label column sm="4">
                     Number of Trees
+                </Form.Label>
+                <Col sm="8" className='rd__wrapper'>
+                    <Form.Control type="text" placeholder="text" sm="4"/>
+                </Col>
+
+            </Form.Group>
+            <Form.Group as={Row} controlId="formPlaintextPassword">
+                <Form.Label column sm="4">
+                    Avg tree density, per sq.km
                 </Form.Label>
                 <Col sm="8">
                     <Form.Control type="text" placeholder="text" />
@@ -18,18 +27,10 @@ const ReforeData = () => {
             </Form.Group>
             <Form.Group as={Row} controlId="formPlaintextPassword">
                 <Form.Label column sm="4">
-                     Avg Number of Trees
-                </Form.Label>
-                <Col sm="8">
-                    <Form.Control type="text" placeholder="sq.km" />
-                </Col>
-            </Form.Group>
-            <Form.Group as={Row} controlId="formPlaintextPassword">
-                <Form.Label column sm="4">
                   Tree species
                 </Form.Label>
                 <Col sm="8">
-                    <Form.Control type="text" placeholder="sq.km" />
+                    <Form.Control type="text" placeholder="text" />
                 </Col>
             </Form.Group>
             <Form>
