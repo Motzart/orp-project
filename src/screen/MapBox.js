56be3ce9e90a93c0a7b9c6267a701b7bdbe7d49f
Yurii Oleshko <yurii.oleshko@gmail.com>
2021-02-05 12:12:06 +0200

    hot fix vol5

--- src/screen/MapBox.js
+++ src/screen/MapBox.js
@@ -1,13 +1,13 @@
 import React, {useState, useEffect, useRef} from 'react';
 import mapboxgl from 'mapbox-gl'
-// import MapboxDraw from "@mapbox/mapbox-gl-draw";
-// import area from '@turf/area';
-// import centerOfMass from '@turf/center-of-mass';
+import MapboxDraw from "@mapbox/mapbox-gl-draw";
+import area from '@turf/area';
+import centerOfMass from '@turf/center-of-mass';
 import {Container} from 'react-bootstrap'
-// import {Link} from "react-router-dom";
+import {Link} from "react-router-dom";
 import 'mapbox-gl/dist/mapbox-gl.css';
-// import API from '../api';
-// import olc  from '../utills/openlocal'
+import API from '../api';
+import olc  from '../utills/openlocal'
 const styles = {
     width: "100vw",
     height: "calc(100vh - 140px)",
@@ -17,12 +17,12 @@ const styles = {
 
 const MapBox = () => {
     const [map, setMap] = useState(null);
-    // const [datePolygon, setDataPolygon] = useState({
-    //     region: '',
-    //     polygon: [],
-    //     codePlus: '',
-    //     square: null
-    // });
+    const [datePolygon, setDataPolygon] = useState({
+        region: '',
+        polygon: [],
+        codePlus: '',
+        square: null
+    });
     const [coord, setCoord] = useState({
         lng: 5,
         lat: 34,
@@ -58,48 +58,48 @@ const MapBox = () => {
                 })
             });
 
-            // let draw = new MapboxDraw({
-            //     displayControlsDefault: false,
-            //     controls: {
-            //         polygon: true,
-            //         trash: true
-            //     }
-            // });
-            // map.addControl(draw);
+            let draw = new MapboxDraw({
+                displayControlsDefault: false,
+                controls: {
+                    polygon: true,
+                    trash: true
+                }
+            });
+            map.addControl(draw);
 
-            // map.on('draw.create', updateArea);
-            // map.on('draw.delete', updateArea);
-            // map.on('draw.update', updateArea);
+            map.on('draw.create', updateArea);
+            map.on('draw.delete', updateArea);
+            map.on('draw.update', updateArea);
 
-//             async function updateArea(e) {
-//                 let data = draw.getAll();
-//                 let center = centerOfMass(e.features[0]).geometry.coordinates;
-//                 let place = await API.getInfoForMap(center[1], center[0]);
-//                 if (data.features.length > 1) {
-//                     draw.delete(e.features[0].id);
-//                     return false
-//                 }
-//                 if (data.features.length > 0) {
-//                     const areaData = area(data);
-// // // restrict to area to 2 decimal points
-//                     let rounded_area = Math.round(areaData * 100) / 100;
-//                     setDataPolygon({
-//                         region: place.features[0].place_name,
-//                         polygon: data.features,
-//                         codePlus: olc.encode(center[1], center[0]),
-//                         square: rounded_area
-//                     })
-//                 } else {
-//                     setDataPolygon({
-//                         region: '',
-//                         polygon: [],
-//                         codePlus: null,
-//                         square: 0
-//                     })
-//                     if (e.type !== 'draw.delete')
-//                         alert('Use the draw tools to draw a polygon!');
-//                 }
-//             }
+            async function updateArea(e) {
+                let data = draw.getAll();
+                let center = centerOfMass(e.features[0]).geometry.coordinates;
+                let place = await API.getInfoForMap(center[1], center[0]);
+                if (data.features.length > 1) {
+                    draw.delete(e.features[0].id);
+                    return false
+                }
+                if (data.features.length > 0) {
+                    const areaData = area(data);
+// // restrict to area to 2 decimal points
+                    let rounded_area = Math.round(areaData * 100) / 100;
+                    setDataPolygon({
+                        region: place.features[0].place_name,
+                        polygon: data.features,
+                        codePlus: olc.encode(center[1], center[0]),
+                        square: rounded_area
+                    })
+                } else {
+                    setDataPolygon({
+                        region: '',
+                        polygon: [],
+                        codePlus: null,
+                        square: 0
+                    })
+                    if (e.type !== 'draw.delete')
+                        alert('Use the draw tools to draw a polygon!');
+                }
+            }
 
         };
 
@@ -115,16 +115,16 @@ const MapBox = () => {
             <div ref={el => (mapContainer.current = el)} style={styles}></div>
             <div className="calculation-box">
                 <p>Draw a polygon using the draw tools.</p>
-                {/*{datePolygon.square > 0 && <div className='inside__block'>*/}
-                {/*    <span>Square Meters</span>*/}
-                {/*    <span>{datePolygon.square}</span>*/}
-                {/*    <span>Region</span>*/}
-                {/*    <span>{datePolygon.region}</span>*/}
-                {/*    <span>Code Plus</span>*/}
-                {/*    <span>{datePolygon.codePlus}</span>*/}
-                {/*    <Link to='/project-details'className="w-100 btn btn-lg btn-primary">Upload Data</Link>*/}
+                {datePolygon.square > 0 && <div className='inside__block'>
+                    <span>Square Meters</span>
+                    <span>{datePolygon.square}</span>
+                    <span>Region</span>
+                    <span>{datePolygon.region}</span>
+                    <span>Code Plus</span>
+                    <span>{datePolygon.codePlus}</span>
+                    <Link to='/project-details'className="w-100 btn btn-lg btn-primary">Upload Data</Link>
 
-                {/*</div>}*/}
+                </div>}
             </div>
         </div>
         </Container>
